5e82304838705a035ed09a88f9c5bd37f006fc52
Ahmed Ali <theahmedjoyali@gmail.com>
2025-12-19 17:21:23 -0800

    The Summary page completed, working, and marking the end of Phase 2

---
+++ src/app/summary/page.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link"
+import localFont from "next/font/local"
+import Demographics from "../components/Demographics"
+import RadialChart from "../components/RadicalChart"
+import BackButton from "../components/UI/BackButton"
+import ProceedButton from "../components/UI/ProceedButton"
+
+const roobertFontRegular = localFont({
+  src: "../fonts/RoobertTRIAL-Regular.woff2",
+  weight: "400"
+})
+
+const roobertFontSemiBold = localFont({
+  src: "../fonts/RoobertTRIAL-SemiBold.woff2",
+  weight: "600"
+})
+
+export default function Summary() {
+  return (
+    <div className="h-screen md:h-[90vh] flex flex-col md:mt-5">
+      <main className="flex-1 w-full bg-white md:overflow-hidden overflow-auto">
+        <div className="md:h-full max-w-full mx-5 px-4 md:px-auto flex flex-col">
+          <div className="text-start ml-4 mb-4 md:mb-10 md:ml-0">
+            <h2 className={`text-base md:text-base font-semibold mb-1 leading-6 ${roobertFontSemiBold.className}`}>
+              A.I. ANALYSIS
+            </h2>
+            <h3 className={`text-4xl md:text-[72px] font-normal leading-16 tracking-tighter ${roobertFontRegular.className}`}>
+              DEMOGRAPHICS
+            </h3>
+            <h4 className={`text-sm mt-2 leading-6 ${roobertFontRegular.className}`}>
+              PREDICTED RACE &amp; AGE
+            </h4>
+          </div>
+
+          <Demographics>
+            <RadialChart />
+          </Demographics>
+
+          <div className="pt-4 md:pt-[37px] pb-6 bg-white sticky bottom-40 md:static md:bottom-0 mb-8 md:mb-16">
+            <div className="flex justify-between max-w-full mx-auto px-4 md:px-0">
+              <Link href="/select">
+                <BackButton />
+              </Link>
+              <Link href="/">
+                <ProceedButton isSummary={false} isHome={true} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
